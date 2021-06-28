114c3ebba8739dcae5de1e5909de86911639b0e3
scottdspangler <scott.spangler@devopsglobalsolutions.com>
2021-06-28 00:41:25 -0700

    First Commit

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,7 +3,7 @@
 # Specify the provider and access details
 provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = "default"
+  profile                 = "DOGS_Profile_Acct_493127450650:IAM-user/sspangler"
   region                  = var.aws_region
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,12 +17,14 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "bradfordhamilton/crystal_blockchain:latest"
+  default     = "tutum/hello-world:latest"
+  # default     = "bradfordhamilton/crystal_blockchain:latest"
 }
 
 variable "app_port" {
   description = "Port exposed by the docker image to redirect traffic to"
-  default     = 3000
+  # default   = 3000
+  default     = 80
 }
 
 variable "app_count" {
